9e1f777f89da9059e017c9bbf872a6c015a12535
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-11-07 13:43:10 -0600

    create classes and add pseudocode

--- src/main/java/com/pluralsight/util/ReceiptWriter.java
+++ src/main/java/com/pluralsight/util/ReceiptWriter.java
@@ -1,5 +1,29 @@
 package com.pluralsight.util;
 
-public class ReceiptWriter {
+import com.pluralsight.model.Order;
 
+import java.io.FileWriter;
+
+public class ReceiptWriter {
+    public static void saveReceipt(Order order) {
+        // generate filename using current date/time (yyyyMMdd-HHmmss.txt)
+        String fileName = generateTimestamp() + ".txt";
+        // create a FileWriter and wrap in BufferedWriter (src/main/resources/receipts)
+
+        // loop through all order items
+        //   - write the items to the to reciept
+        try {
+            FileWriter writer = new FileWriter();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        //write total cost
+
+        // close BufferedWriter
+        // handle IOException with error message
+    }
+
+    private static String generateTimestamp() {
+        // Create timestamp string useing a formatter
+        // return that string
 }

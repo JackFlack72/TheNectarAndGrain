6c0432d55701e984b004d58dd9559217af1d6124
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-11-10 09:28:30 -0600

    add constructor, getters, getPrice to Chips

--- src/main/java/com/pluralsight/model/Chips.java
+++ src/main/java/com/pluralsight/model/Chips.java
@@ -1,4 +1,17 @@
 package com.pluralsight.model;
 
 public class Chips extends Order{
+    private String chipsType;
+
+    public Chips(String chipsType) {
+        this.chipsType = chipsType;
+    }
+
+    public String getChipsType() {
+        return chipsType;
+    }
+
+    public double getPrice() {
+        return 1.50;
+    }
 }

a66da22685ff7d9e938a10afd25be530d4a7d8b5
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-11-10 09:10:03 -0600

    add constructor and getters to Drink

--- src/main/java/com/pluralsight/model/Drink.java
+++ src/main/java/com/pluralsight/model/Drink.java
@@ -1,4 +1,20 @@
 package com.pluralsight.model;
 
 public class Drink extends Order{
+    private String drinkSize;
+    private String drinkFlavor;
+
+    public Drink(String drinkSize, String drinkFlavor) {
+        this.drinkSize = drinkSize;
+        this.drinkFlavor = drinkFlavor;
+    }
+
+    public String getDrinkSize() {
+        return drinkSize;
+    }
+
+    public String getDrinkFlavor() {
+        return drinkFlavor;
+    }
+
 }
